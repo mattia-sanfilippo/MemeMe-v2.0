126102966956e62986ef53c48cea99fb335e9378
Mattia Sanfilippo <imattolo@me.com>
2020-04-07 15:59:15 +0200

    Initial Commit

---
+++ Pick Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pick Test
+//
+//  Created by Mattia Sanfilippo on 07/04/2020.
+//  Copyright © 2020 Mattia Sanfilippo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
